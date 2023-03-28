5867326cde4cd18144d23c8e6fe012dc2f511d13
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-03-28 15:33:39 +0900

    created specific functions for user Struct

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -26,16 +25,14 @@ func Register(c *fiber.Ctx) error {
 			"message": "passwords do not match",
 		})
 	}
-
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
-
 	user := models.User{
 		FirstName: data["first_name"],
 		LastName:  data["last_name"],
 		Email:     data["email"],
 		IsMithril: false,
-		Password:  string(password),
 	}
+
+	user.SetPassword(data["password"])
 
 	database.DB.Create(&user)
 
@@ -60,7 +57,7 @@ func Login(c *fiber.Ctx) error {
 		}))
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"])); err != nil {
+	if err := user.CompareHashedPassword(data["password"]); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON((fiber.Map{
 			"message": "Wrong Password",
--- src/models/user.go
+++ src/models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id        uint
 	FirstName string
@@ -7,4 +9,13 @@ type User struct {
 	Email     string
 	Password  string
 	IsMithril bool
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+	user.Password = string(hashedPassword)
+}
+
+func (user *User) CompareHashedPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 }
