23f31e82245c61d178ff651e2d60a90c2008d722
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-03-28 15:50:28 +0900

    added unique constraint to email

--- src/models/user.go
+++ src/models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	Id        uint   `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
+	Email     string `json:"email" gorm:"unique"`
 	Password  string `json:"-"`
 	IsMithril bool   `json:"-"`
 }
